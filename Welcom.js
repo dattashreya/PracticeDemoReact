0584201f2c4c6cda249e0365462c37f49638c2c4
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-25 23:05:53 +0530

    without using JSX style
    without using JSX style

---
+++ Welcom.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcom = () => {
+  {/* <>
+    <h4>Greeting here</h4>
+    <Welcome />
+  </> */}
+  return React.createElement('div',null, 'welcom');
+}
+
+
+export default Welcom;
