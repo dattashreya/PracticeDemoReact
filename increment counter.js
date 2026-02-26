353a04040b65e7be71fa87fd42b1f854898a47be
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-02-26 18:18:26 +0530

    increment counter.js
    increment counter.js

---
+++ increment counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './style.css';
+import React, {  Component } from 'react';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 0
+    }
+  }
+  subsc() {
+    this.setState({
+      message: this.state.message +1
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h4>Count - {this.state.message}</h4>
+        <button onClick={()=> this.subsc()}>click me</button>
+      </div>
+    );
+  }
+}
+
+
